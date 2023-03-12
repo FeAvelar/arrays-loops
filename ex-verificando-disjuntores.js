68ec2133f85073abdd1356dd308a5140eaafb206
Fernando Avelar <fernandoestevaoa@gmail.com>
2023-03-12 11:46:58 -0300

    ex-verificando-disjuntores

---
+++ ex-verificando-disjuntores.js
@@ -0,0 +1,9 @@
+const disjuntores = [false, true, true, false, false, true, false, false, true, true];
+
+for (let i = 0; i < disjuntores.length; i++) {
+    const elemento = disjuntores[i];
+    if (disjuntores[i] === true) {
+        console.log(i);
+    }
+     
+}
